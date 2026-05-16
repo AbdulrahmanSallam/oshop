7dfe5a4f6cc32152f665bc801f7c72bf3c491ccc
Abdulrahman Sallam <abdulrahman.sallam.official@gmail.com>
2026-05-16 12:06:04 +0300

    add routing with navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BsNavbarComponent } from './layout/bs-navbar/bs-navbar.component';
+import { ShoppingCartComponent } from './pages/shopping-cart/shopping-cart.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { CheckOutComponent } from './pages/check-out/check-out.component';
+import { OrderSuccessComponent } from './pages/order-success/order-success.component';
+import { MyOrdersComponent } from './pages/my-orders/my-orders.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BsNavbarComponent,
+    ShoppingCartComponent,
+    ProductsComponent,
+    CheckOutComponent,
+    OrderSuccessComponent,
+    MyOrdersComponent,
+    AdminProductsComponent,
+    AdminOrdersComponent,
+    HomeComponent,
+    LoginComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
